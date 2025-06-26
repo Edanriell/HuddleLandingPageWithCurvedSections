a78fa73ba8617242bf5b08618e945d1d01775289
Edanriell <Edanriell@gmail.com>
2025-06-26 19:33:11 +0300

    feature(header): created header

---
+++ Client/src/widgets/header/ui/header.svelte
@@ -0,0 +1,11 @@
+<script>
+	import {Logotype} from "@shared/ui/logotype";
+	import {Link} from "@shared/ui/link";
+</script>
+
+<header class="bg-[#fff]">
+	<div class="relative z-2 flex flex-row items-center justify-between pr-[24rem] pl-[24rem] mt-[24rem] mb-[24rem] desktop:pr-[80rem] desktop:pl-[63rem] desktop:mt-[78rem] desktop:mb-[78rem]">
+		<Logotype colorScheme="Dark" />
+		<Link color="" hoverColor="" href="/" type="ghost">Try It Free</Link>
+	</div>
+</header>
