78e30524cb90b442b26baea8c45d1797c840b8a9
Edanriell <Edanriell@gmail.com>
2025-06-26 19:21:54 +0300

    feature(home-page): created home page

---
+++ Client/src/app/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import {HomePage} from "@pages/home";
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta content="Huddle" name="description" />
+</svelte:head>
+
+<HomePage />
---
+++ Client/src/pages/home/ui/home-page.svelte
@@ -0,0 +1,10 @@
+<!--<script>-->
+<!--	import {HeroWidget} from "@widgets/hero/ui";-->
+<!--	import {FeaturesWidget} from "@widgets/features/ui";-->
+<!--	import {ActionsWidget} from "@widgets/actions/ui";-->
+<!--</script>-->
+
+<!--<HeroWidget />-->
+<!--<StatisticsWidget />-->
+<!--<FeaturesWidget />-->
+<!--<ActionsWidget />-->
