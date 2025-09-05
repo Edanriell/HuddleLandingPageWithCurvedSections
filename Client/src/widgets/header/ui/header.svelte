310950cd3f28d3d542f37c1d434946a7ab228b08
Edanriell <Edanriell@gmail.com>
2025-09-05 13:12:42 +0300

    feature(link): implemented link component

--- Client/src/widgets/header/ui/header.svelte
+++ Client/src/widgets/header/ui/header.svelte
@@ -6,6 +6,6 @@
 <header class="bg-[#fff]">
 	<div class="relative z-2 flex flex-row items-center justify-between pr-[24rem] pl-[24rem] mt-[24rem] mb-[24rem] desktop:pr-[80rem] desktop:pl-[63rem] desktop:mt-[78rem] desktop:mb-[78rem]">
 		<Logotype colorScheme="Dark" />
-		<Link color="" hoverColor="" href="/" type="ghost">Try It Free</Link>
+		<Link color="" height="" hoverColor="" href="/" type="ghost" width="">Try It Free</Link>
 	</div>
 </header>
