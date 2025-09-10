6601eee9b1659ac15efb054e3392af809c7f3073
Edanriell <Edanriell@gmail.com>
2025-09-10 12:53:26 +0300

    bugfix(footer-general-fix): finished footer

--- Client/src/widgets/footer/ui/footer.svelte
+++ Client/src/widgets/footer/ui/footer.svelte
@@ -8,16 +8,16 @@
 	import {default as FooterMaskDesktop} from "./footer-mask-desktop.svelte";
 </script>
 
-<footer class="bg-[#00252e] flex flex-col-reverse pt-[108rem] pb-[46rem] gap-y-[104rem] relative">
+<footer class="bg-[#00252e] flex flex-col-reverse pt-[108rem] pb-[46rem] gap-y-[104rem] relative desktop:flex-row desktop:gap-x-[unset] desktop:gap-y-[unset] desktop:pt-[280rem] desktop:pb-[103rem] desktop:items-start desktop:justify-between">
 	<FooterMaskMobile />
 	<FooterMaskDesktop />
-	<div class="ml-[28rem] mr-[28rem]">
-		<Logotype class="relative mb-[16rem] block" colorScheme="Light" />
+	<div class="ml-[28rem] mr-[28rem] desktop:ml-[120rem] desktop:mr-[unset] desktop:basis-[340rem]">
+		<Logotype class="relative mb-[16rem] block desktop:mb-[27rem]" colorScheme="Light" />
 		<p class="font-[var(--font-family)] font-semibold text-[14rem] leading-[171%] text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nulla quam, hendrerit lacinia vestibulum a, ultrices quis sem.</p>
 		<Contacts />
 		<SocialLinks classes="ml-[-3rem]" />
 	</div>
-	<div class="ml-[28rem] mr-[28rem]">
+	<div class="ml-[28rem] mr-[28rem] desktop:ml-[unset] desktop:mr-[120rem] desktop:basis-[520rem]">
 		<Newsletter />
 	</div>
 </footer>
