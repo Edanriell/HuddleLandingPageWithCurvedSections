aa7832ff33f90c723763a1c581d744b132b3d23a
Edanriell <Edanriell@gmail.com>
2025-09-11 11:54:50 +0300

    feature(hero): created mobile hero widget

---
+++ Client/src/widgets/hero/ui/hero-widget-illustration.svelte
@@ -0,0 +1,235 @@
+<svg class="block ml-[auto] mr-[auto] w-[296rem] h-[209rem] tablet:w-[600rem] tablet:h-[420rem] desktop:w-[1016rem] desktop:h-[720rem]" fill="none" height="739" viewBox="0 0 1035 739" width="1035" xmlns="http://www.w3.org/2000/svg">
+	<g id="Group 41">
+		<g id="Group 10">
+			<g filter="url(#filter0_d_0_360)" id="Rectangle 3">
+				<rect fill="white" height="600" rx="20" width="960" x="10" y="10"/>
+			</g>
+			<g filter="url(#filter1_d_0_360)" id="Rectangle 3_2">
+				<path d="M10 30C10 18.9543 18.9543 10 30 10H950C961.046 10 970 18.9543 970 30V74H10V30Z" fill="#00252E"/>
+			</g>
+			<rect fill="#E1E8EE" height="24" id="Rectangle 5" rx="5" width="200" x="82" y="30"/>
+			<g id="Group 5">
+				<rect fill="#D8D8D8" height="4" id="Rectangle 6" rx="2" width="32" x="874" y="32"/>
+				<rect fill="#D8D8D8" height="4" id="Rectangle 6 Copy" rx="2" width="32" x="874" y="40"/>
+				<rect fill="#D8D8D8" height="4" id="Rectangle 6 Copy 2" rx="2" width="32" x="874" y="48"/>
+			</g>
+			<g id="Group 9">
+				<circle cx="130" cy="196" fill="url(#paint0_linear_0_360)" id="Oval Copy" r="48"/>
+				<g id="Group 4">
+					<rect fill="#E1E8EE" height="16" id="Rectangle 4" rx="3" width="115" x="234" y="160"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy" rx="3" width="400" x="234" y="192"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 2" rx="3" width="366.667" x="234" y="208"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 3" rx="3" width="333.333" x="234" y="224"/>
+				</g>
+				<g id="Group 6">
+					<circle cx="694" cy="196" fill="#A7B9C8" id="Oval" r="12"/>
+					<circle cx="758" cy="196" fill="#A7B9C8" id="Oval Copy 5" r="12"/>
+					<circle cx="726" cy="196" fill="#A7B9C8" id="Oval Copy 3" r="12"/>
+					<circle cx="790" cy="196" fill="#A7B9C8" id="Oval Copy 6" r="12"/>
+					<circle cx="822" cy="196" fill="#A7B9C8" id="Oval Copy 8" r="12"/>
+				</g>
+			</g>
+			<g id="Group 8">
+				<circle cx="127" cy="329" fill="url(#paint1_linear_0_360)" id="Oval Copy 4" r="45"/>
+				<g id="Group 4 Copy">
+					<rect fill="#E1E8EE" height="16" id="Rectangle 4_2" rx="3" width="115" x="231" y="293"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy_2" rx="3" width="400" x="231" y="325"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 2_2" rx="3" width="366.667" x="231" y="341"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 3_2" rx="3" width="333.333" x="231" y="357"/>
+				</g>
+				<g id="Group 6 Copy">
+					<circle cx="691" cy="329" fill="#A7B9C8" id="Oval_2" r="12"/>
+					<circle cx="755" cy="329" fill="#A7B9C8" id="Oval Copy 5_2" r="12"/>
+					<circle cx="723" cy="329" fill="#A7B9C8" id="Oval Copy 3_2" r="12"/>
+				</g>
+			</g>
+			<g id="Group 7">
+				<circle cx="127" cy="472" fill="url(#paint2_linear_0_360)" id="Oval Copy 2" r="45"/>
+				<g id="Group 4 Copy 2">
+					<rect fill="#E1E8EE" height="16" id="Rectangle 4_3" rx="3" width="115" x="231" y="436"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy_3" rx="3" width="400" x="231" y="468"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 2_3" rx="3" width="366.667" x="231" y="484"/>
+					<rect fill="#E1E8EE" height="8" id="Rectangle 4 Copy 3_3" rx="3" width="333.333" x="231" y="500"/>
+				</g>
+				<g id="Group 6 Copy 2">
+					<circle cx="691" cy="472" fill="#A7B9C8" id="Oval_3" r="12"/>
+					<circle cx="755" cy="472" fill="#A7B9C8" id="Oval Copy 5_3" r="12"/>
+					<circle cx="723" cy="472" fill="#A7B9C8" id="Oval Copy 3_3" r="12"/>
+					<circle cx="787" cy="472" fill="#A7B9C8" id="Oval Copy 6_2" r="12"/>
+				</g>
+			</g>
+		</g>
+		<g id="Group 40">
+			<g id="Group 3">
+				<g filter="url(#filter2_d_0_360)" id="Rectangle 3_3">
+					<path clip-rule="evenodd" d="M706 235.067C706 225.641 713.641 218 723.067 218H1008.93C1018.36 218 1026 225.641 1026 235.067V712.933C1026 722.359 1018.36 730 1008.93 730H723.067C713.641 730 706 722.359 706 712.933V235.067Z" fill="white" fill-rule="evenodd"/>
+				</g>
+				<g filter="url(#filter3_d_0_360)" id="Rectangle 3_4">
+					<path clip-rule="evenodd" d="M706 235.067C706 225.641 713.641 218 723.067 218H1008.93C1018.36 218 1026 225.641 1026 235.067V272.613H706V235.067Z" fill="#00252E" fill-rule="evenodd"/>
+				</g>
+			</g>
+			<g id="Group 12">
+				<rect fill="#E1E8EE" height="20.48" id="Rectangle 5_2" rx="4.26667" width="104" x="738.133" y="236.773"/>
+				<g id="Group 5_2">
+					<rect fill="#D8D8D8" height="3.41333" id="Rectangle 6_2" rx="1.70667" width="27.3067" x="964.56" y="238.48"/>
+					<rect fill="#D8D8D8" height="3.41333" id="Rectangle 6 Copy_2" rx="1.70667" width="27.3067" x="964.56" y="245.307"/>
+					<rect fill="#D8D8D8" height="3.41333" id="Rectangle 6 Copy 2_2" rx="1.70667" width="27.3067" x="964.56" y="252.133"/>
+				</g>
+			</g>
+			<g id="Group 11">
+				<circle cx="753" cy="341" fill="url(#paint3_linear_0_360)" id="Oval Copy_2" r="15"/>
+				<g id="Group 4_2">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_4" rx="0.919612" width="57.5" x="784" y="329"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_4" rx="0.919612" width="200" x="784" y="339.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_4" rx="0.919612" width="183.333" x="784" y="345"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_4" rx="0.919612" width="166.667" x="784" y="350.333"/>
+				</g>
+			</g>
+			<g id="Group 33">
+				<circle cx="753" cy="479" fill="url(#paint4_linear_0_360)" id="Oval Copy 7" r="15"/>
+				<g id="Group 4 Copy 5">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_5" rx="0.919612" width="57.5" x="784" y="467"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_5" rx="0.919612" width="200" x="784" y="477.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_5" rx="0.919612" width="183.333" x="784" y="483"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_5" rx="0.919612" width="166.667" x="784" y="488.333"/>
+				</g>
+			</g>
+			<g id="Group 13">
+				<g id="Group 4 Copy 3">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_6" rx="0.919612" width="57.5" x="784" y="375"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_6" rx="0.919612" width="200" x="784" y="385.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_6" rx="0.919612" width="183.333" x="784" y="391"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_6" rx="0.919612" width="166.667" x="784" y="396.333"/>
+				</g>
+				<circle cx="753" cy="387" fill="url(#paint5_linear_0_360)" id="Oval Copy 4_2" r="15"/>
+			</g>
+			<g id="Group 34">
+				<g id="Group 4 Copy 6">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_7" rx="0.919612" width="57.5" x="784" y="513"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_7" rx="0.919612" width="200" x="784" y="523.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_7" rx="0.919612" width="183.333" x="784" y="529"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_7" rx="0.919612" width="166.667" x="784" y="534.333"/>
+				</g>
+				<circle cx="753" cy="525" fill="url(#paint6_linear_0_360)" id="Oval Copy 9" r="15"/>
+			</g>
+			<g id="Group 14">
+				<g id="Group 4 Copy 4">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_8" rx="0.919612" width="57.5" x="784" y="421"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_8" rx="0.919612" width="200" x="784" y="431.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_8" rx="0.919612" width="183.333" x="784" y="437"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_8" rx="0.919612" width="166.667" x="784" y="442.333"/>
+				</g>
+				<circle cx="753" cy="433" fill="url(#paint7_linear_0_360)" id="Oval Copy 2_2" r="15"/>
+			</g>
+			<g id="Group 37">
+				<g id="Group 4 Copy 7">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_9" rx="0.919612" width="57.5" x="784" y="559"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_9" rx="0.919612" width="200" x="784" y="569.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_9" rx="0.919612" width="183.333" x="784" y="575"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_9" rx="0.919612" width="166.667" x="784" y="580.333"/>
+				</g>
+				<circle cx="753" cy="571" fill="url(#paint8_linear_0_360)" id="Oval Copy 10" r="15"/>
+			</g>
+			<g id="Group 38">
+				<circle cx="753" cy="617" fill="url(#paint9_linear_0_360)" id="Oval Copy 13" r="15"/>
+				<g id="Group 4 Copy 10">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_10" rx="0.919612" width="57.5" x="784" y="605"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_10" rx="0.919612" width="200" x="784" y="615.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_10" rx="0.919612" width="183.333" x="784" y="621"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_10" rx="0.919612" width="166.667" x="784" y="626.333"/>
+				</g>
+			</g>
+			<g id="Group 39">
+				<g id="Group 4 Copy 9">
+					<rect fill="#E1E8EE" height="5.33333" id="Rectangle 4_11" rx="0.919612" width="57.5" x="784" y="651"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy_11" rx="0.919612" width="200" x="784" y="661.667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 2_11" rx="0.919612" width="183.333" x="784" y="667"/>
+					<rect fill="#E1E8EE" height="2.66667" id="Rectangle 4 Copy 3_11" rx="0.919612" width="166.667" x="784" y="672.333"/>
+				</g>
+				<circle cx="753" cy="663" fill="url(#paint10_linear_0_360)" id="Oval Copy 12" r="15"/>
+			</g>
+		</g>
+	</g>
+	<defs>
+		<filter color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="620" id="filter0_d_0_360" width="980" x="0" y="0">
+			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+			<feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+			<feOffset/>
+			<feGaussianBlur stdDeviation="5"/>
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.145098 0 0 0 0 0.180392 0 0 0 0.181839 0"/>
+			<feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_0_360"/>
+			<feBlend in="SourceGraphic" in2="effect1_dropShadow_0_360" mode="normal" result="shape"/>
+		</filter>
+		<filter color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="84" id="filter1_d_0_360" width="980" x="0" y="0">
+			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+			<feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+			<feOffset/>
+			<feGaussianBlur stdDeviation="5"/>
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.145098 0 0 0 0 0.180392 0 0 0 0.181839 0"/>
+			<feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_0_360"/>
+			<feBlend in="SourceGraphic" in2="effect1_dropShadow_0_360" mode="normal" result="shape"/>
+		</filter>
+		<filter color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="530" id="filter2_d_0_360" width="338" x="697" y="209">
+			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+			<feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+			<feOffset/>
+			<feGaussianBlur stdDeviation="4.5"/>
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.145098 0 0 0 0 0.180392 0 0 0 0.181839 0"/>
+			<feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_0_360"/>
+			<feBlend in="SourceGraphic" in2="effect1_dropShadow_0_360" mode="normal" result="shape"/>
+		</filter>
+		<filter color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="72.6133" id="filter3_d_0_360" width="338" x="697" y="209">
+			<feFlood flood-opacity="0" result="BackgroundImageFix"/>
+			<feColorMatrix in="SourceAlpha" result="hardAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+			<feOffset/>
+			<feGaussianBlur stdDeviation="4.5"/>
+			<feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0.145098 0 0 0 0 0.180392 0 0 0 0.181839 0"/>
+			<feBlend in2="BackgroundImageFix" mode="normal" result="effect1_dropShadow_0_360"/>
+			<feBlend in="SourceGraphic" in2="effect1_dropShadow_0_360" mode="normal" result="shape"/>
+		</filter>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint0_linear_0_360" x1="28.1704" x2="86.9075" y1="155.901" y2="291.998">
+			<stop stop-color="#FAD961"/>
+			<stop offset="1" stop-color="#FF52C1"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint1_linear_0_360" x1="82" x2="82" y1="284" y2="374">
+			<stop stop-color="#00C7FA"/>
+			<stop offset="1" stop-color="#007DFA"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint2_linear_0_360" x1="58.0159" x2="113.12" y1="454.552" y2="554.671">
+			<stop stop-color="#FF52C1"/>
+			<stop offset="1" stop-color="#9952FF"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint3_linear_0_360" x1="721.178" x2="739.534" y1="328.469" y2="370.999">
+			<stop stop-color="#FAD961"/>
+			<stop offset="1" stop-color="#FF52C1"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint4_linear_0_360" x1="721.178" x2="739.534" y1="466.469" y2="508.999">
+			<stop stop-color="#FAD961"/>
+			<stop offset="1" stop-color="#FF52C1"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint5_linear_0_360" x1="738" x2="738" y1="372" y2="402">
+			<stop stop-color="#00C7FA"/>
+			<stop offset="1" stop-color="#007DFA"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint6_linear_0_360" x1="738" x2="738" y1="510" y2="540">
+			<stop stop-color="#00C7FA"/>
+			<stop offset="1" stop-color="#007DFA"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint7_linear_0_360" x1="730.005" x2="748.373" y1="427.184" y2="460.557">
+			<stop stop-color="#FF52C1"/>
+			<stop offset="1" stop-color="#9952FF"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint8_linear_0_360" x1="730.005" x2="748.373" y1="565.184" y2="598.557">
+			<stop stop-color="#FF52C1"/>
+			<stop offset="1" stop-color="#9952FF"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint9_linear_0_360" x1="721.178" x2="739.534" y1="604.469" y2="646.999">
+			<stop stop-color="#FAD961"/>
+			<stop offset="1" stop-color="#FF52C1"/>
+		</linearGradient>
+		<linearGradient gradientUnits="userSpaceOnUse" id="paint10_linear_0_360" x1="738" x2="738" y1="648" y2="678">
+			<stop stop-color="#00C7FA"/>
+			<stop offset="1" stop-color="#007DFA"/>
+		</linearGradient>
+	</defs>
+</svg>
---
+++ Client/src/widgets/hero/ui/hero.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import {Link} from "@shared/ui/link";
+
+	import {default as HeroWidgetIllustration} from "./hero-widget-illustration.svelte";
+</script>
+
+<section class="relative pr-[40rem] pl-[40rem] mt-[88rem]">
+	<h1 class="font-[var(--second-family)] font-bold text-[24rem] leading-[150%] text-[#00252e] text-center mt-[unset]! mb-[24rem]!">Build The Community Your Fans Will Love</h1>
+	<p class="font-[var(--font-family)] font-normal text-[14rem] leading-[150%] text-[#00252e] text-center mb-[48rem]! text-balance">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
+	<Link
+			bgColor="#ff52c1"
+			bgHoverColor="#ff8ed7"
+			borderRadius={20}
+			classes="text-center block! ml-auto mr-auto leading-[38rem] desktop:leading-[78rem] mb-[104rem]"
+			color="#fff"
+			height={40}
+			hoverColor="#fff"
+			hoverShadow="0 6rem 13rem 0 rgba(0, 37, 46, 0.22)"
+			href="/"
+			screenLargeBorderRadius={40}
+			screenLargeHeight={80}
+			screenLargeTextSize={20}
+			screenLargeWidth={400}
+			shadow="0 3rem 7rem 0 rgba(0, 37, 46, 0.22)"
+			shape="pill"
+			textSize={12}
+			textWeight={700}
+			type="solid"
+			width={200}
+	>Get Started For Free</Link>
+	<HeroWidgetIllustration />
+</section>
