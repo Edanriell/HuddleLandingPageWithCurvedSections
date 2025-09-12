100f1890efceaa2bde54c782e5e016c51c795175
Edanriell <Edanriell@gmail.com>
2025-09-12 13:04:52 +0300

    feature(statistics-widget): created desktop version of widget

--- Client/src/widgets/statistics/ui/statistics-widget.svelte
+++ Client/src/widgets/statistics/ui/statistics-widget.svelte
@@ -4,16 +4,16 @@
 
 <section class="relative mt-[120rem]">
 	<h2 class="visually-hidden">Statistics</h2>
-	<dl class="flex flex-col items-center gap-y-[88rem] ml-auto mr-auto">
+	<dl class="flex flex-col items-center gap-y-[88rem] desktop:flex-row desktop:justify-center desktop:gap-x-[341rem]">
 		<div class="flex flex-col-reverse items-center">
-			<PeopleIcon classes="order-[1] self-start mb-[-11rem]" />
-			<dt class="font-[var(--font-family)] font-normal text-[15rem] leading-[150%] text-[#808e9a] mt-[1rem]">Communities Formed</dt>
-			<dd class="font-[var(--font-family)] font-bold text-[60rem] leading-[150%] text-[#00252e]">1.4k+</dd>
+			<PeopleIcon classes="order-[1] self-start mb-[-11rem] desktop:mb-[-16rem]" />
+			<dt class="font-[var(--font-family)] font-normal text-[15rem] leading-[150%] text-[#808e9a] mt-[1rem] desktop:mt-[2rem] desktop:text-[24rem]">Communities Formed</dt>
+			<dd class="font-[var(--font-family)] font-bold text-[60rem] leading-[150%] text-[#00252e] desktop:text-[96rem]">1.4k+</dd>
 		</div>
 		<div class="flex flex-col-reverse items-center">
-			<MessageIcon classes="order-[1] self-start mb-[-9rem]" />
-			<dt class="font-[var(--font-family)] font-normal text-[15rem] leading-[150%] text-[#808e9a] mt-[1rem]">Messages Sent</dt>
-			<dd class="font-[var(--font-family)] font-bold text-[60rem] leading-[150%] text-[#00252e]">2.7m+</dd>
+			<MessageIcon classes="order-[1] self-start mb-[-9rem] desktop:mb-[-16rem]" />
+			<dt class="font-[var(--font-family)] font-normal text-[15rem] leading-[150%] text-[#808e9a] mt-[1rem] desktop:mt-[2rem] desktop:text-[24rem]">Messages Sent</dt>
+			<dd class="font-[var(--font-family)] font-bold text-[60rem] leading-[150%] text-[#00252e] desktop:text-[96rem]">2.7m+</dd>
 		</div>
 	</dl>
 </section>
