0d242dc13829e9fb325cfeaedfa4a8abe13029f7
Edanriell <Edanriell@gmail.com>
2025-09-17 10:32:14 +0300

    feature(ptomotion-widget): finished widget

--- Client/src/pages/home/ui/home-page.svelte
+++ Client/src/pages/home/ui/home-page.svelte
@@ -2,8 +2,10 @@
 	import {Hero} from "@widgets/hero";
 	import {Statistics} from "@widgets/statistics";
 	import {Features} from "@widgets/features";
+	import {Promotion} from "@widgets/promotion";
 </script>
 
 <Hero />
 <Statistics />
 <Features />
+<Promotion />
